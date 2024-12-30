4b48a8661760a1ac98985a8667a29caf12e42925
matija <matija@matija-VirtualBox>
2024-12-30 23:18:05 +0100

    While Schleife, unendlich

---
+++ linuxProjekt/src/linuxProjekt/whileSchleife.java
@@ -0,0 +1,17 @@
+package linuxProjekt;
+
+public class whileSchleife {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int  i=1;
+		while (i>0)	{
+			++i;
+			System.out.println("Count "+ i);
+		}
+		System.out.println("False");
+	}
+
+}
