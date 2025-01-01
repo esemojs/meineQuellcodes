2ea48c5169ddb161a363200ed8a5e2b6a2c4b679
matija <matija@matija-VirtualBox>
2025-01-01 15:15:48 +0100

    prüfung email adresse

--- linuxProjekt/src/linuxProjekt/passwortabfrage.java
+++ linuxProjekt/src/linuxProjekt/passwortabfrage.java
@@ -7,13 +7,16 @@ public class passwortabfrage {
 	public static void main(String[] args) {
 		Scanner eingabe = new Scanner(System.in);
 		int passwort, fehler = 3;
+		String email;
 		do {
+			System.out.println("E-Mail Adresse eingeben");
+			email = eingabe.next().toLowerCase();
+			
 			System.out.println("Passwort eingeben");
 			passwort = eingabe.nextInt();
 			--fehler;
-			if (passwort != 100) {
-				System.out.printf("Nach 3 falschen Versuchen wird ihr Konto gesperrt. Sie haben noch %d versuche\n",
-						fehler);
+			if ((passwort != 100) || (!(email.equals("esemojs")))) {
+				System.out.printf("Fehlerhafte Credentials. Sie haben noch %d versuche\n", fehler);
 				if (0 == fehler) {
 					System.err.println("Ihr Konto wurde gesperrt");
 					System.out.println("Wenden Sie sich an den Administrator");
@@ -22,7 +25,7 @@ public class passwortabfrage {
 				}
 			}
 
-		} while (passwort != 100);
+		} while ((passwort != 100) || (!(email.equals("esemojs"))));
 		System.out.println("Sie werden eingeloggt");
 	}
 }
